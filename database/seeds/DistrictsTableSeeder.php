a1d4705d666216bd0269f47e09472316293da094
zhaochang <80330582@163.com>
2018-07-05 00:10:58 +0800

    调整地区假数据

--- database/seeds/DistrictsTableSeeder.php
+++ database/seeds/DistrictsTableSeeder.php
@@ -11,8 +11,47 @@ class DistrictsTableSeeder extends Seeder
      */
     public function run()
     {
+//        \App\Models\District::truncate();
+//        $intentions = factory(\App\Models\District::class,20)->make();
+//        \App\Models\District::insert($intentions->toArray());
+        \Illuminate\Support\Facades\DB::update('SET FOREIGN_KEY_CHECKS=0');
         \App\Models\District::truncate();
-        $intentions = factory(\App\Models\District::class,20)->make();
-        \App\Models\District::insert($intentions->toArray());
+
+        $data = [
+            [
+                'name' => '北京',
+                'parent_id' => 0,
+            ],
+            [
+                'name' => '河北省',
+                'parent_id' => 0,
+            ],
+            [
+                'name' => '朝阳区',
+                'parent_id' => 1,
+            ],
+            [
+                'name' => '东城区',
+                'parent_id' => 1,
+            ],
+            [
+                'name' => '昌平区',
+                'parent_id' => 1,
+            ],
+            [
+                'name' => '石家庄',
+                'parent_id' => 2,
+            ],
+            [
+                'name' => '固安',
+                'parent_id' => 2,
+            ],
+            [
+                'name' => '沧州',
+                'parent_id' => 2,
+            ],
+        ];
+
+        \App\Models\District::insert($data);
     }
 }
