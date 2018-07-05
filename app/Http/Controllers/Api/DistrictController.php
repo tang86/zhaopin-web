cc3ce0721ff082a7edfaadcd396302d437af4ade
zhaochang <80330582@163.com>
2018-07-05 23:34:34 +0800

    1

---
+++ app/Http/Controllers/Api/DistrictController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\District;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DistrictController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        $where = [
+            'parent_id' => 0
+        ];
+        $districts = District::where($where)
+            ->with('districts')
+            ->get();
+
+
+        foreach ($districts as $district) {
+            $data['all'][] = $district;
+            $data['provinces'][$district->id] = $district->name;
+            $data['cities'][$district->id] = $district->districts;
+        }
+
+        return $this->sendResponse($data, '获取成功');
+    }
+}
