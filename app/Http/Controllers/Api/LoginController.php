ad5fc3e0752641ffa5985f97c05255905723c11f
zhaochang <80330582@163.com>
2018-07-19 16:42:22 +0800

    恢复目录结构

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\LoginRequest;
+use App\Models\CreditConfig;
+use App\Models\UserPointsLog;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -39,19 +41,35 @@ class LoginController extends Controller
             return $this->sendResponse(['_token' => $token, 'user' => $user], '登陆成功');
         }
         // 不存在添加用户、返回私人令牌
+        $inviter_id = $request->inviter_id??null;
         $user = User::create([
             'open_id' => $request->open_id,
             'union_id' => $request->union_id??null,
             'name' => $request->name,
             'head_url' => $request->head_url,
+            'inviter_id' => $inviter_id
         ]);
 
         if (empty($user)) {
             return $this->sendError('登录失败', ['用户注册失败'], 500);
         }
+        if (!is_null($inviter_id)) {
+            //奖励邀请人
 
+            $credit_config = CreditConfig::find(2);
+            $inviter = User::find($inviter_id);
+            $code = "invite_{$inviter_id}";
+
+            if (UserPointsLog::canIAdd($inviter->id, $credit_config, $code)) {
+                $inviter->points += $credit_config->points;
+                $inviter->save();
+                UserPointsLog::add($inviter_id, $credit_config, $code);
+
+            }
+
+        }
         // 获取令牌
         $token = $user->createToken($request->open_id)->accessToken;
-        return $this->sendResponse(['_token' => $token, 'user' => $user], '登陆成功');
+        return $this->sendResponse(['_token' => $token, 'user' => $user , 'isFirst' => true], '登陆成功');
     }
 }
